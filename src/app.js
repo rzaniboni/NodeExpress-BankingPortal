4323bbe06bbfc8c79a3bd8f1faf0e48e6bba68a7
Roberto Zaniboni <zaniboni@gmail.com>
2018-08-02 01:19:25 +0200

    add index.ejs

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
-const fs = require('fs')
-const path = require('path')
-const express = require('express')
-const app = express()
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
