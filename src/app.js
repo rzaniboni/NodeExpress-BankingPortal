43e79351f0dedc0a0bb035091708bdb40a67287a
Roberto Zaniboni <zaniboni@gmail.com>
2018-08-02 01:22:28 +0200

    create a server

--- src/app.js
+++ src/app.js
@@ -7,3 +7,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Index' });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log('PS Project Running on port ${port}!');
+});
